df855d53950f7ec0a5455d04758a738a67120856
caucricity@gmail.com <caucricity@gmail.com@b30ce7b0-8ac7-e8dc-b454-b21cbf033d92>
2014-11-18 16:15:25 +0000

    fin de creation de DB

--- trunk/creation.sql
+++ trunk/creation.sql
@@ -10,21 +10,22 @@ USE rrl_test;
 -- Les tables
 
 -- 1
-CREATE TABLE menbre(
-       id        INTEGER UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-       nom       VARCHAR(12),
-       prenom    VARCHAR(12),
-       mail      VARCHAR(32),
-       telephone INTEGER(10),
-       adresse   INTEGER REFERENCES id(adresse));
-
--- 2
 CREATE TABLE adresse(
        id          INTEGER     NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
        ville       VARCHAR(12) NOT NULL,
        rue         VARCHAR(16) NOT NULL,
        code_postal INTEGER(5)  NOT NULL,
        pays        VARCHAR(12) NOT NULL);
+
+-- 2
+CREATE TABLE menbre(
+       id        INTEGER UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+       nom       VARCHAR(12),
+       prenom    VARCHAR(12),
+       mail      VARCHAR(32),
+       telephone INTEGER(10),
+       adresse   INTEGER,
+       FOREIGN KEY (adresse) REFERENCES adresse(id) ON DELETE CASCADE);
 
 -- 3
 CREATE TABLE catalogue(
@@ -35,45 +36,60 @@ CREATE TABLE catalogue(
 -- 4
 CREATE TABLE promo(
        id        INTEGER NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-       CATALOGUE INTEGER REFERENCES id(catalogue),
+       catalogue INTEGER,
        reduction INTEGER,
-       CONSTRAINT cred CHECK (reduction between 0 and 100)); 
+       CONSTRAINT Cred CHECK (reduction BETWEEN 0 AND 100),
+       FOREIGN KEY (catalogue) REFERENCES catalogue(id) ON DELETE CASCADE);
 
 -- 5
-CREATE TABLE commande(
-       id       INTEGER NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-       nprod    INTEGER REFERENCES id(produit),
-       quantite INTEGER,
-       nclient  INTEGER REFERENCES id(menbre)); 
 
--- 6
 CREATE TABLE produit(
        id          INTEGER NOT NULL UNIQUE PRIMARY KEY,
        designation VARCHAR(32),
        descriptif  VARCHAR(64),
        disponible  INTEGER,
        prix        INTEGER,
-       catalogue   INTEGER REFERENCES id(catalogue));
+       catalogue   INTEGER, 
+       FOREIGN KEY (catalogue) REFERENCES catalogue(id) ON DELETE CASCADE);
     
--- 7   
-CREATE TABLE expidition(
-       id     INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
-       n_addr INTEGER REFERENCES id(adresse),
-       date   DATE);
+-- 6
 
+CREATE TABLE commande(
+       id       INTEGER NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+       nprod    INTEGER,
+       quantite INTEGER,
+       nclient  INTEGER,
+       FOREIGN KEY (nprod)   REFERENCES produit(id) ON DELETE CASCADE,
+       FOREIGN KEY (nclient) REFERENCES menbre(id) ON UPDATE CASCADE); 
+
+-- 7   
+CREATE TABLE expedition(
+       id     INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+       addr   INTEGER NOT NULL, 
+       dexp   DATE,
+       FOREIGN KEY (addr) REFERENCES adresse(id)  ON DELETE CASCADE);
+
+-- 8 
+CREATE TABLE code_promo(
+       ncl         INTEGER NOT NULL,
+       pourcentage INTEGER NOT NULL,
+       ncmd        INTEGER,    
+       FOREIGN KEY (ncl)  REFERENCES menbre(id)   ON DELETE CASCADE,
+       FOREIGN KEY (ncmd) REFERENCES commande(id) ON DELETE CASCADE);
+       
 
 -- Les contraintes specifiques 
 
--- 8
+-- 9
 ALTER TABLE menbre ADD CONSTRAINT Cmail
       CHECK (mail LIKE '%@%');
 
--- 9
+-- 10
 ALTER TABLE adresse ADD CONSTRAINT Cpays
       CHECK (pays IN ('ALLEMAGNE', 'FRANCE', 'ITALIE', 'ESPAGNE', 'BELGIQUE')); 
 
--- 10
-
-
 -- 11
+
+
+
 
